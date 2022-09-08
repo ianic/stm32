10c17a7be40dbd6006eca28b3049fc528f1e2dc9
Igor Anic <ianic@inet.hr>
2022-09-08 13:19:23 +0200

    add regz submodule

--- scripts/cgen.sh
+++ scripts/cgen.sh
@@ -4,4 +4,13 @@ set -e
 git_root=$(git rev-parse --show-toplevel)
 cd $git_root/cgen
 
+if [[ ! -f regz/zig-out/bin/regz ]]; then
+   cd regz
+   zig build
+   cd ..
+fi
+
 ./gpio.rb 411re | zig fmt --stdin > ../chips/stm32f411re/gpio.zig
+
+# TODO: have some modifications in generated file
+# regz/zig-out/bin/regz ./data/STM32F411.svd > ../chips/stm32f411re/registers.zig
