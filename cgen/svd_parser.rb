e19cde075544500992da834a6a0abe2ab8e3db85
Igor Anić <igor.anic@gmail.com>
2022-09-13 18:21:17 +0200

    make elements in svd parser copy-able

--- cgen/svd_parser.rb
+++ cgen/svd_parser.rb
@@ -19,6 +19,16 @@ class Document
     pd.attribute(name.to_s).text
   end
 
+  def elem(m)
+    m = :description if m.to_sym == :desc
+    path = m.to_s.camelcase
+    return nil if @doc.xpath(path).empty?
+    text = @doc.xpath(path).text
+    text = text.delete("\n").squeeze(" ") if m == :description
+    text.fix_hex
+    text.is_integer? ? text.to_i : text
+  end
+
   def method_missing(m, *args, &block)
     m = :description if m.to_sym == :desc
     path = m.to_s.camelcase
@@ -33,19 +43,33 @@ class Document
 end
 
 class Field < Document
-  def enum
-    @enum ||= begin
-                evs = @doc.xpath("enumeratedValues")
-                return nil if evs.empty?
+  attr_reader :name, :desc, :bit_offset, :bit_width, :enum
 
-                name = if evs.attribute("derivedFrom")
-                         evs.map{ |e| e.attribute("derivedFrom").value}.join("_")
-                       else
-                         evs.map{ |e| e.xpath("name").text}.join("_")
-                       end
-                Enum.new(name, evs.xpath("enumeratedValue"), self)
-              end
+  def initialize(doc, parent)
+    super(doc, parent)
+    @name = elem(:name)
+    @desc = elem(:desc)
+    @bit_offset = elem(:bit_offset)
+    @bit_width = elem(:bit_width)
+    @enum = begin
+              evs = doc.xpath("enumeratedValues")
+              return nil if evs.empty?
+              name = if evs.attribute("derivedFrom")
+                       evs.map{ |e| e.attribute("derivedFrom").value}.join("_")
+                     else
+                       evs.map{ |e| e.xpath("name").text}.join("_")
+                     end
+              Enum.new(name, evs.xpath("enumeratedValue"), self)
+            end
   end
+
+  private
+
+  def initialize_dup(orig)
+    @enum = orig.enum.dup
+    super
+  end
+
 end
 
 class Enum
@@ -108,16 +132,20 @@ class PaddingField
 end
 
 class Register < Document
-  attr_reader :address, :bit_width, :type
+  attr_reader  :name, :desc, :address_offset, :size,  :bit_width, :type, :dim, :fields, :reset_value
 
   def initialize(doc, parent)
     super(doc, parent)
-    self.fields
+    @name = elem(:name)
+    @desc = elem(:desc)
+    @address_offset = elem(:address_offset)
+    @size = as_int(elem(:size) || 32)
+    @reset_value = elem(:reset_value)
+    @dim = (elem(:dim) || 0).to_i
+    @fields = add_reserved_padding(@doc.xpath("fields/field").map{ |fd| Field.new(fd, self) })
   end
 
-  def fields
-    @fields ||= add_reserved_padding(@doc.xpath("fields/field").map{ |fd| Field.new(fd, self) })
-  end
+  private
 
   def add_reserved_padding(fields)
     if fields.length == 1  and fields.first.bit_offset == 0 and fields.first.name == self.name
@@ -148,16 +176,22 @@ class Register < Document
     return fields_with_padding
   end
 
-  def dim
-    d = @doc.xpath("dim")
-    return 0 if d.empty?
-    d.text.to_i
+  def initialize_dup(orig)
+    @fields = orig.fields.map{ |f| f.dup } if orig.fields
+    super
   end
 
-  def size
-    return 32 if @doc.xpath("size").empty?
-    as_int(@doc.xpath("size").text.fix_hex)
+  public
+
+  def derived(name, address_offset, desc, parent)
+    d = self.dup
+    d.instance_variable_set(:@name, name)
+    d.instance_variable_set(:@address_offset, address_offset)
+    d.instance_variable_set(:@desc, desc) if desc
+    d.instance_variable_set(:@parent, parent)
+    d
   end
+
 end
 
 def as_int(str)
@@ -177,59 +211,42 @@ class Child
   end
 end
 
-class ClusterRegister < Child
-  attr_reader :name, :address_offset, :address
-
-  def initialize(orig, name, offset, parent)
-    @orig = orig
-    @name = name
-    @address_offset = "0x" + (orig.address_offset.to_i(16) + offset).to_s(16)
-    @parent = parent
-  end
-end
-
-class DerivedRegister < Child
-  attr_reader :name, :address_offset, :desc, :address
-
-  def initialize(orig, name, address_offset, desc, parent)
-    @orig = orig
-    @name = name
-    @address_offset = address_offset
-    @desc = desc
-    @parent = parent
-  end
-end
-
 class Peripheral < Document
-  def registers
-    @registers ||= all_registers
+  attr_reader :name, :desc, :base_address, :registers
+
+  def initialize(doc)
+    super(doc, nil)
+    @name = elem(:name)
+    @desc = elem(:desc)
+    @base_address = elem(:base_address)
+    @registers = all_registers
   end
 
   private
 
   def all_registers
     regs = @doc.xpath("registers/register").map do |d|
-      next unless d.attribute("derivedFrom").nil? # only derived
+      next unless d.attribute("derivedFrom").nil?
       Register.new(d, self)
     end.compact
     ((regs + cluster_registers).flatten.
       map{ |r| r.dim > 0 ? dim_duplicate(r) : r }.flatten +
-      derived(regs)
+      derived_registers(regs)
     ).compact
   end
 
-  def derived(regs)
+  def derived_registers(regs)
     @doc.xpath("registers/register").map do |rd|
       next if rd.attribute("derivedFrom").nil? # only derived
       from_name = rd.attribute("derivedFrom").text
       orig = regs.find{ |p| p.name == from_name }
       raise "orig #{from_name} not found for #{rd.xpath('name').text}" unless orig
-      DerivedRegister.new(orig,
-                          rd.xpath("name").text,
-                          rd.xpath("addressOffset").text.fix_hex,
-                          rd.xpath("description").text,
-                          self
-                         )
+      orig.derived(
+        rd.xpath("name").text,
+        rd.xpath("addressOffset").text.fix_hex,
+        rd.xpath("description").text,
+        self
+      )
     end.compact
   end
 
@@ -239,7 +256,11 @@ class Peripheral < Document
     subs = r.dim_index.split(",")
     (0..dim-1).map do |i|
       sub = subs[i].split("-")[0] # fix for "1-1" string for TIM10.CCR
-      ClusterRegister.new(r, r.name.sub("%s", sub), increment * i, self)
+      r.derived(
+        r.name.sub("%s", sub),
+        "0x" + (r.address_offset.to_i(16) + increment * i).to_s(16),
+        nil,
+        self)
     end
   end
 
@@ -255,18 +276,26 @@ class Peripheral < Document
 
     (0..dim-1).map do |i|
       prefix = name.sub("%s", index[i])
-      regs.map{ |r| ClusterRegister.new(r, prefix + r.name, address_offset + increment * i, self) }
+      regs.map{ |r| r.derived(
+                  prefix + r.name,
+                  "0x" + (r.address_offset.to_i(16) + address_offset + increment * i).to_s(16),
+                  nil,
+                  self) }
     end.flatten
   end
-end
 
-class DerivedPeripheral < Child
-  attr_reader :name, :base_address
+  def initialize_dup(orig)
+    @registers = orig.registers.map{ |r| r.dup }
+    super
+  end
 
-  def initialize(orig, name, base_address)
-    @orig = orig
-    @name = name
-    @base_address = base_address
+  public
+
+  def derived(name, base_address)
+    d = self.dup
+    d.instance_variable_set(:@name, name)
+    d.instance_variable_set(:@base_address, base_address)
+    d
   end
 end
 
@@ -275,21 +304,18 @@ class Device < Document
     @peripherals ||= all_peripherals
   end
 
-  def recurring_enums
+  def calc_enum_redirect_to
     keys = {}
     self.enums.each do |e|
-      if e.name == "MODER0"
-        print "#{e.key} #{e.path} #{keys[e.key].nil?} #{e.object_id}\n"
+      if keys[e.key].nil?
+        keys[e.key] = e
+      else
+        e.redirect_to = keys[e.key]
       end
-      # if !keys[e.key].nil?
-      #   o = keys[e.key]
-      #   e.redirect_to = o unless e.object_id == o.object_id
-      # else
-      #   keys[e.key] = e
-      #   e.redirect_to = nil
-      # end
     end
   end
+
+  private
 
   def enums
     return @enums if @enums
@@ -306,221 +332,25 @@ class Device < Document
     @enums
   end
 
-  private
-
   def all_peripherals
     regular = @doc.xpath("//peripheral").map do |d|
       next unless d.attribute("derivedFrom").nil? # skip derived
       Peripheral.new(d)
     end.compact
-    (regular + derived(regular)).flatten
+    (regular + derived_peripherals(regular)).flatten
   end
 
   # if derivedFrom is defined on peripheral
   # copy from orig except name and base_address
-  def derived(regular)
+  def derived_peripherals(regular)
     @doc.xpath("//peripheral").map do |pd|
       next if pd.attribute("derivedFrom").nil? # only derived
       from_name = pd.attribute("derivedFrom").text
       orig = regular.find{ |p| p.name == from_name }
       raise "orig #{from_name} not found for #{pd.xpath('name').text}" unless orig
-      DerivedPeripheral.new(orig, pd.xpath("name").text, pd.xpath("baseAddress").text.fix_hex)
+      orig.derived(pd.xpath("name").text, pd.xpath("baseAddress").text.fix_hex)
     end.compact
   end
 end
 
 
-class SvdParser
-
-  def initialize(chip)
-    filename = "data/stm32f#{chip[0..2].upcase}.svd"
-    filename = "data/test.svd" if $is_test
-    #filename = "data/STM32F#{chip[0..2].upcase}.svd"
-    @doc = File.open(filename) { |f| Nokogiri::XML(f) }
-    @peripherals = []
-
-    Device.new(@doc)
-  end
-
-  # xml doc to hash
-  def peripheral(pd)
-    {
-      "name" => pd.xpath("name").text,
-      "desc" => pd.xpath("description").text.delete("\n").squeeze(" "),
-      "group" => pd.xpath("groupName").text,
-      "base_address" => pd.xpath("baseAddress").text.sub("0X", "0x").downcase,
-    }
-  end
-
-  def register(rd)
-    {
-      "name" => rd.xpath("name").text,
-      "desc" => rd.xpath("description").text.delete("\n").squeeze(" "),
-      "address_offset" => rd.xpath("addressOffset").text.sub("0X", "0x").downcase,
-      "access" => rd.xpath("access").text,
-      "size" => rd.xpath("size").text.to_i(16),
-      "reset_value" => rd.xpath("resetValue").text,
-
-      "dim" => rd.xpath("dim").text.to_i,
-      "dim_increment" => rd.xpath("dimIncrement").text.to_i(16),
-      "dim_index" => rd.xpath("dimIndex").text
-    }
-  end
-
-  def field(fd)
-    f  = {
-      "name" => fd.xpath("name").text,
-      "desc" => fd.xpath("description").text.delete("\n").squeeze(" "),
-      "bit_offset" => fd.xpath("bitOffset").text.to_i,
-      "bit_width" => fd.xpath("bitWidth").text.to_i,
-    }
-    # evs = fd.xpath("enumeratedValues")
-    # unless evs.empty?
-    #   f["enum"] = self.enum(evs)
-    # end
-    f
-  end
-
-  def enum(evs)
-    return nil if evs.nil?
-    if evs.attribute("derivedFrom")
-      return {
-        "name" => evs.map{ |e| e.attribute("derivedFrom").value}.join("_"),
-      }
-    end
-    {
-      "name" => evs.map{ |e| e.xpath("name").text}.join("_"),
-      "values" => evs.xpath("enumeratedValue").map do |ev|
-        {
-          "name" => ev.xpath("name").text,
-          "desc" => ev.xpath("description").text.delete("\n").squeeze(" "),
-          "value" => ev.xpath("value").text.to_i,
-        }
-      end,
-    }
-  end
-
-  def registers(rp)
-    rp.map do |rd|
-      r = self.register(rd)
-      r["fields"] = rd.xpath("fields/field").map{ |fd| self.field(fd) }
-      r["dim"] == 0 ? r : self.dim_duplicate(r)
-    end.flatten
-  end
-
-  def cluster(pd)
-    return [] if pd.xpath("registers/cluster").empty?
-    cd = pd.xpath("registers/cluster")
-    dim = cd.xpath("dim").text.to_i
-    increment = cd.xpath("dimIncrement").text.to_i(16)
-    address_offset = cd.xpath("addressOffset").text.to_i(16)
-    index = cd.xpath("dimIndex").text.split(",")
-    name = cd.xpath("name").text
-    regs = registers(cd.xpath("register"))
-    cluster_regs = []
-    (0..dim-1).map do |i|
-      prefix = name.sub("%s", index[i])
-      cluster_regs << regs.map{ |r|
-        r = r.clone()
-        r["name"] = prefix + r["name"]
-        r["address_offset"] = "0x" + (r["address_offset"].to_i(16) + address_offset + increment * i).to_s(16)
-        r
-      }
-    end
-    cluster_regs.flatten
-  end
-
-  def regular
-    @peripherals = @doc.xpath("//peripheral").map do |pd|
-      next unless pd.attribute("derivedFrom").nil? # skip derived
-      p = self.peripheral(pd)
-      p["registers"] = (registers(pd.xpath("registers/register")) + cluster(pd)).flatten
-      p
-    end.compact
-  end
-
-  def dim_duplicate(r)
-    dim = r["dim"]
-    increment = r["dim_increment"]
-    subs = r["dim_index"].split(",")
-
-    (0..dim-1).map do |i|
-      c = r.clone
-      sub = subs[i].split("-")[0] # fix for "1-1" string for TIM10.CCR
-      c["name"] = c["name"].sub("%s", sub)
-      c["address_offset"] = "0x" + (c["address_offset"].to_i(16) + increment * i).to_s(16)
-      c.delete "dim"
-      c.delete "dim_increment"
-      c.delete "dim_index"
-      c
-    end
-  end
-
-  def derived
-    for pd in @doc.xpath("//peripheral") do
-      # if derivedFrom is defined on peripheral
-      # copy from parent except name and base_address
-      next if pd.attribute("derivedFrom").nil? # only derived
-      from_name = pd.attribute("derivedFrom").text
-      parent = @peripherals.find{ |p| p["name"] == from_name }
-      raise "parent #{from_name} not found for #{pd.xpath('name').text}" unless parent
-      p = parent.clone.merge({
-                               "name" => pd.xpath("name").text,
-                               "base_address" => pd.xpath("baseAddress").text.sub("0X", "0x").downcase,
-                             })
-      @peripherals << p
-    end
-  end
-
-  def parse
-    self.regular
-    self.derived
-    add_calulated_fields @peripherals.to_o
-    #@po
-    #add_calulated_fields
-    #self.hash_to_object
-  end
-
-  def add_calulated_fields(peripherals)
-    peripherals.map do |p|
-      p.registers.map! do |r|
-        r.address = "0x" + (p.base_address.to_i(16) + r.address_offset.to_i(16)).to_s(16)
-        self.add_reserved_padding(r)
-        r
-      end
-      p
-    end
-  end
-
-  def add_reserved_padding(r)
-    if r.fields.length == 1  and r.fields.first.bit_offset == 0 and r.fields.first.name == r.name
-      r.bit_width = r.fields.first.bit_width
-      r.type = r.bit_width == 32 ? :int : :mmio_int
-      return
-    end
-
-    r.fields.sort_by!(&:bit_offset)
-    fields = []
-    bit_offset = 0
-    for f in r.fields do
-      if f.bit_offset != bit_offset
-        fields << OpenStruct.new({
-                                   "name" => "reserved_#{bit_offset}_#{f.bit_offset-1}",
-                                   "bit_offset" => bit_offset,
-                                   "bit_width" => f.bit_offset - bit_offset,
-                                 })
-      end
-      fields << f
-      bit_offset = f.bit_offset + f.bit_width
-    end
-    if bit_offset < r.size
-      fields << OpenStruct.new({
-                                 "name" => "padding_#{bit_offset}_#{r.size-1}",
-                                 "bit_offset" => bit_offset,
-                                 "bit_width" => r.size - bit_offset,
-                               })
-    end
-    r.fields = fields
-  end
-
-end
